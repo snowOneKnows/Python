05fa1c2ebcd87af29555fd6bac310c5b1d448626
snowOneKnows <110148072+snowOneKnows@users.noreply.github.com>
2022-08-09 20:46:36 +1000

    Update
    load

---
+++ portScanner/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# Port scanning in python using builtins 
+# Imports 
+
+import socket
+# Class 
+class Scanner:
+    def __init__(self, ip):
+        self.ip = ip
+        self.open_ports = [];
+
+
+    def __repr__(self):
+        return 'Scanner: {}'.format(self.ip)
+
+    def add_port(self, port):
+        self.open_ports.append(port)
+        print("IP has port Open: {} {}".format(self.ip,port))
+
+
+    def scan(self, lowerport, upperport):
+        for port in range(lowerport, upperport + 1):
+            if self.is_open(port):
+                self.add_port(port)
+
+
+    def is_open(self, port):
+        """" check if socket is open. this will attempt to connect if is open retun true or false. """
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.4)
+        result = s.connect_ex((self.ip, port))
+        s.close()
+        return result == 0
+
+    def write(self, filepath):
+        openport = map(str, self.open_ports)
+        with open(filepath, 'w') as f:
+            f.write('\n'.join(openport))
+
+def main():
+    ip = '192.168.50.1'
+    scanner = Scanner(ip)
+    scanner.scan(50, 85)
+    scanner.write('ports.txt')
+
+if __name__ == '__main__':
+    main()
